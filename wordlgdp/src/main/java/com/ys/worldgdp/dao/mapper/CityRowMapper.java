96bed039af8a43abeff8ccf6a372aeac34fb68a9
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-07-11 16:59:43 +0530

    RowMapper for City Bean

---
+++ wordlgdp/src/main/java/com/ys/worldgdp/dao/mapper/CityRowMapper.java
@@ -0,0 +1,22 @@
+package com.ys.worldgdp.dao.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.ys.worldgdp.City;
+
+public class CityRowMapper implements RowMapper<City> {
+
+	public City mapRow(ResultSet rs, int rowNum) throws SQLException {
+		City	city	=	new	City();
+		city.setCountryCode(rs.getString("country_code"));
+		city.setName(rs.getString("name"));
+		city.setId(rs.getLong("id"));
+		city.setDistrict(rs.getString("district"));
+		city.setPopulation(rs.getLong("population"));
+		return null;
+	}
+
+}
