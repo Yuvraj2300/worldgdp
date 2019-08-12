44ce07d9f4cfce5453fd6109374c323db516d928
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-08-12 21:23:47 +0530

    CityDAO, still a couple methods to go

---
+++ wordlgdp/src/main/java/com/ys/worldgdp/dao/CityDAO.java
@@ -0,0 +1,56 @@
+package com.ys.worldgdp.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import com.ys.worldgdp.City;
+import com.ys.worldgdp.dao.mapper.CityRowMapper;
+
+import lombok.Setter;
+
+@Setter
+public class CityDAO {
+
+	@Autowired
+	NamedParameterJdbcTemplate namedJdbcTemplate;
+
+	private static final Integer PAGE_SIZE = 20;
+
+	//OVERLOADED METHOD TO HANDLE PAGE NO NULL
+	public List<City> getCities(String countryCode) {
+		return getCities(countryCode, null);
+	}
+	
+	public List<City> getCities(String countryCode, Integer pageNo) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("code", countryCode);
+		if (pageNo != null) {
+			Integer offset = (pageNo - 1) * PAGE_SIZE;
+			params.put("offset", offset);
+			params.put("size", PAGE_SIZE);
+		}
+		
+		return namedJdbcTemplate.query("SELECT"
+				+ "ID,NAME,COUNTRYCODE COUNTRY_CODE,DISTRICT,POPULATION "
+				+ "FROM CITY WHERE COUNTRYCODE = :code"
+				+ "ORDER BY POPULATION DESC"
+				+ ((pageNo != null) ? "LIMIT:offset, :size" : ""),
+			params, new CityRowMapper());
+	}
+	
+	public City getCityDetails(String cityId) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("id", cityId);
+		
+		return namedJdbcTemplate.queryForObject("SELECT id,"
+				+ "name,countrycode country_code,"
+				+ "district,population"
+				+ "from city where id = :id", 
+			params, new CityRowMapper());
+	}	
+
+}
