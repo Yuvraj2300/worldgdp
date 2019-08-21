7f1c285740c6d6a62f64ecc6632e03108c58ed84
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-08-21 12:08:12 +0530

    Added AppConfiguration for MVC

---
+++ wordlgdp/src/main/java/com/ys/worldgdp/config/AppConfiguration.java
@@ -0,0 +1,17 @@
+package com.ys.worldgdp.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.ys.worldgdp" })
+public class AppConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+}
