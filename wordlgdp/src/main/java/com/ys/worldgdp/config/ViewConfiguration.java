b10deae8051c2048b3ed0b637a613791995fe48d
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-08-26 16:54:51 +0530

    Added template engine :steam_locomotive:

--- wordlgdp/src/main/java/com/ys/worldgdp/config/ViewConfiguration.java
+++ wordlgdp/src/main/java/com/ys/worldgdp/config/ViewConfiguration.java
@@ -2,19 +2,33 @@ package com.ys.worldgdp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 @Configuration
 public class ViewConfiguration {
-	
+
 	@Bean
 	public ClassLoaderTemplateResolver templateResolver() {
-		ClassLoaderTemplateResolver	resolver	=	new	ClassLoaderTemplateResolver();
+		ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
 		resolver.setPrefix("templates/");
 		resolver.setSuffix(".html");
 		resolver.setTemplateMode(TemplateMode.HTML);
 		resolver.setCacheable(false);
-		return	resolver;
+		return resolver;
 	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setDialect(new LayoutDialect());
+		return templateEngine;
+	}
+	
+	
 }
