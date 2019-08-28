e7c77ed9d1ace26249ea19bb453a3d87646f0616
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-08-28 16:07:04 +0530

    Instead of @RestController . use @Controller

--- wordlgdp/src/main/java/com/ys/worldgdp/controller/view/ViewController.java
+++ wordlgdp/src/main/java/com/ys/worldgdp/controller/view/ViewController.java
@@ -3,18 +3,18 @@ package com.ys.worldgdp.controller.view;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.ys.worldgdp.dao.CityDAO;
 import com.ys.worldgdp.dao.CountryDAO;
 import com.ys.worldgdp.dao.LookupDAO;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class ViewController {
 
