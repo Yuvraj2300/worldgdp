05c3d9c8250aaeb4b7224ea2e3e8f8fba0f793ab
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-08-28 16:05:50 +0530

    Instead of setting the dialect, add a new dialect.

--- wordlgdp/src/main/java/com/ys/worldgdp/config/ViewConfiguration.java
+++ wordlgdp/src/main/java/com/ys/worldgdp/config/ViewConfiguration.java
@@ -1,9 +1,12 @@
 package com.ys.worldgdp.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.thymeleaf.TemplateEngine;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -12,12 +15,12 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @Configuration
-public class ViewConfiguration {
+public class ViewConfiguration{
 
 	@Bean
 	public ClassLoaderTemplateResolver templateResolver() {
 		ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
-		resolver.setPrefix("templates/");
+		resolver.setPrefix("/templates/");
 		resolver.setSuffix(".html");
 		resolver.setTemplateMode(TemplateMode.HTML);
 		resolver.setCacheable(false);
@@ -28,14 +31,16 @@ public class ViewConfiguration {
 	public SpringTemplateEngine templateEngine() {
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setTemplateResolver(templateResolver());
-		templateEngine.setDialect(new LayoutDialect());
+		//templateEngine.setDialect(new LayoutDialect());
+		templateEngine.addDialect(new LayoutDialect());
 		return templateEngine;
 	}
-	
-	public	ViewResolver	getViewResolver() {
-		ThymeleafViewResolver	thymVR	=	new	ThymeleafViewResolver();
+
+	@Bean
+	public ViewResolver getViewResolver() {
+		ThymeleafViewResolver thymVR = new ThymeleafViewResolver();
 		thymVR.setTemplateEngine(templateEngine());
 		thymVR.setCharacterEncoding("UTF-8");
-		return	thymVR;
+		return thymVR;
 	}
 }
